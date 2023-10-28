470daecacd844dca5878ff6dbb84b80ce4148f15
RobsonMCambraia <robson.mcambraia23@gmail.com>
2023-10-28 02:48:10 -0300

    "comentários"

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-// Efeito máquina de texto
-function typeWrite(elemento){
+// Função para criar um efeito de digitação de texto
+function typeWrite(elemento) {
+    // Divide o texto em um array de letras
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
-      
-    setTimeout(function(){
-        elemento.innerHTML += letra;
-    }, 75 * i)
-
-  });
+    
+    // Itera pelas letras do array e exibe uma a uma com atraso
+    textoArray.forEach(function (letra, i) {
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 75 * i);
+    });
 }
+
+// Seleciona o elemento 'h1' e aplica o efeito de digitação
 const titulo = document.querySelector('h1');
 typeWrite(titulo);
 
+// Função para adicionar um compromisso
 function addAppointment() {
+    // Obtém os valores dos campos de entrada
     const name = document.getElementById("name").value;
     const date = document.getElementById("date").value;
     const time = document.getElementById("time").value;
     const notes = document.getElementById("notes").value;
 
+    // Cria um objeto de compromisso com os valores obtidos
     const appointment = {
         id: Date.now(),
         name,
@@ -27,26 +33,38 @@ function addAppointment() {
         notes
     };
 
+    // Obtém a lista de compromissos do armazenamento local ou cria um array vazio
     let appointments = JSON.parse(localStorage.getItem("appointments")) || [];
+    
+    // Adiciona o novo compromisso ao array
     appointments.push(appointment);
+    
+    // Armazena o array atualizado de compromissos no armazenamento local
     localStorage.setItem("appointments", JSON.stringify(appointments))
 
+    // Exibe os compromissos na lista
     displayAppointments();
 }
 
+// Função para exibir os compromissos na lista
 function displayAppointments() {
-    const appointments = JSON.parse(localStorage.getItem("appointments")) || []
+    // Obtém a lista de compromissos do armazenamento local ou cria um array vazio
+    const appointments = JSON.parse(localStorage.getItem("appointments")) || [];
     const list = document.getElementById("appointment-list");
+    
+    // Limpa o conteúdo da lista
     list.innerHTML = '';
 
-    appointments.forEach(appointment =>{
+    // Itera pelos compromissos e adiciona cada um à lista
+    appointments.forEach(appointment => {
         list.innerHTML += `
         <li>
             ${appointment.name} - ${appointment.date} - ${appointment.time}
             <p>${appointment.notes}</p>
         </li>
         `;
-    })
+    });
 }
 
+// Exibe os compromissos ao carregar a página
 window.onload = displayAppointments;
